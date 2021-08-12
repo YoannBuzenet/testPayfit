00ab7281f4019d82e07ee6047e976323bdc17321
Yoann <ybuzenet@gmail.com>
2021-08-12 16:36:41 +0200

    loading comments for each post

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -9,11 +9,28 @@ const Post = ({ match, history }) => {
     (post) => post.id == match.params.id
   );
 
+  // Getting Comments for that post
+  const comments = globalInformations.comments.filter(
+    (comment) => comment.postId == match.params.id
+  );
+
   return (
     <div className="container">
       <div>
         <p>{currentPost?.title}</p>
         <p>{currentPost?.body}</p>
+      </div>
+
+      <div className="postComment">
+        {comments.map((comment) => {
+          return (
+            <div>
+              <p>{comment.name}</p>
+              <p>{comment.body}</p>
+              <p>{comment.email}</p>
+            </div>
+          );
+        })}
       </div>
 
       <div className="backToHome">
