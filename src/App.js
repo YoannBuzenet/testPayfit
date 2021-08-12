7b7aede4f7bcb3a23421e79579e0b5e232d22555
Yoann <ybuzenet@gmail.com>
2021-08-12 15:51:08 +0200

    router implementation

--- src/App.js
+++ src/App.js
@@ -14,9 +14,16 @@ import postsAPI from "./services/posts";
 import Comments from "./components/Comments/Comments/Comments";
 import Posts from "./components/Posts/Posts/Posts";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Comment from "./components/Comments/Comment/Comment";
+
 function App() {
   //State initialization for futures Contexts
-  const [globalInformations, setGlobalInformations] = useState({});
+  const [globalInformations, setGlobalInformations] = useState({
+    users: [],
+    comments: [],
+    posts: [],
+  });
 
   //Context initialization
   const globalInformationsDefinition = {
@@ -25,6 +32,7 @@ function App() {
   };
 
   useEffect(() => {
+    console.log("test yo");
     const allPosts = postsAPI.getAllPosts();
     const allUsers = usersAPI.getAllUsers();
     const allComments = commentsAPI.getAllComments();
@@ -42,13 +50,24 @@ function App() {
 
   return (
     <GlobalContext.Provider value={globalInformationsDefinition}>
-      <div className="App">
-        <header className="App-header"></header>
-        <div>
-          <Posts />
-          <p>Ok</p>
-        </div>
-      </div>
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <div className="App">
+                <header className="App-header"></header>
+                <div>
+                  <Posts />
+                  <p>Ok</p>
+                </div>
+              </div>
+            )}
+          />
+          <Route path="/comment" exact render={(props) => <Comment />} />
+        </Switch>
+      </Router>
     </GlobalContext.Provider>
   );
 }
--- src/components/Posts/Posts/Posts.jsx
+++ src/components/Posts/Posts/Posts.jsx
@@ -5,6 +5,8 @@ import Post from "../Post/Post";
 const Posts = (arrayOfPosts) => {
   const { globalInformations } = useContext(GlobalInformationsContext);
 
+  console.log("pouet", globalInformations);
+
   return (
     <>
       {globalInformations.posts.map((post) => (
