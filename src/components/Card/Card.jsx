2e833b19cafe91153499a99bf48bd3e98f37d1ce
Yoann <ybuzenet@gmail.com>
2021-08-12 17:32:56 +0200

    css homepage

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function ImgMediaCard({ item, author, pictureURL }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://picsum.photos/200/300"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {item.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            By {author}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/Posts/Posts/Posts.jsx
+++ src/components/Posts/Posts/Posts.jsx
@@ -1,21 +1,23 @@
 import { useContext } from "react";
 import GlobalInformationsContext from "../../../context/globalContext";
 import utils from "../../../services/utils";
+import ImgMediaCard from "../../Card/Card";
 import "./style.css";
 
 const Posts = ({ history }) => {
   const { globalInformations } = useContext(GlobalInformationsContext);
 
   return (
-    <>
+    <div className="postsContainer">
       {globalInformations.posts.map((post) => (
         <div onClick={(e) => history.push(`/post/${post.id}`)}>
-          <span className="postTitle">
-            {utils.capitalizeFirstLetter(post.title)}
-          </span>
+          <ImgMediaCard
+            item={post}
+            author={globalInformations.users[post.userId].email}
+          />
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
